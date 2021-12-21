a0a2ab69a64e0663f55db4757823437b4bea101d
mwelford2 <83231271+mwelford2@users.noreply.github.com>
2021-12-21 13:13:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ client.on("message", async message => {
 })
 
 //random number?
-function getRandomIntInclusive(min, max) {x
+function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
