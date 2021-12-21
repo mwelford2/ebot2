fb9fc279dba72aba313aaea20cc40f749853eb36
mwelford2 <83231271+mwelford2@users.noreply.github.com>
2021-12-21 16:33:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,26 @@ client.on("message", async message => {
             message.channel.send(str);
         }
     }
+
+    function getRandomMessage(m){
+        if (m == 0){
+            getRandomMessage(getRandomIntInclusive(1,4));
+        }
+        if (m == 4){
+            getRandomMessage(getRandomIntInclusive(0,3));
+        }
+        if (m == 1){
+            message.channel.send("Is that a JOJO reference!!!!");
+        }
+        if (m == 2){
+            message.channel.send("I think thats a rick roll, very sus ಠಿ_ಠ");
+        }
+        if (m == 3){
+            message.channel.send("SSSSSSPPPPPPAAAAAAMMMMM");
+        }
+        return;
+    }
+
     var mess = message.content.toLowerCase();
     if (message.content.toLowerCase().includes("kevin")){
         message.channel.send("https://gfycat.com/coarseneighboringhummingbird-kevin-what");
@@ -60,24 +80,7 @@ client.on("message", async message => {
 })
 
 //function to get a random message for links
-function getRandomMessage(m){
-    if (m == 0){
-        getRandomMessage(getRandomIntInclusive(1,4));
-    }
-    if (m == 4){
-        getRandomMessage(getRandomIntInclusive(0,3));
-    }
-    if (m == 1){
-        message.channel.send("Is that a JOJO reference!!!!");
-    }
-    if (m == 2){
-        message.channel.send("I think thats a rick roll, very sus ಠಿ_ಠ");
-    }
-    if (m == 3){
-        message.channel.send("SSSSSSPPPPPPAAAAAAMMMMM");
-    }
-    return;
-}
+
 
 //random number
 function getRandomIntInclusive(min, max) {
