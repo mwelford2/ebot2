e89c785a42fcf8d09dd3164cf7327f91b7a74b37
mwelford2 <83231271+mwelford2@users.noreply.github.com>
2021-12-21 13:24:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,22 +38,31 @@ client.on("message", async message => {
     if (message.content.includes("http://") || message.content.includes("https://")){
         message.channel.send("Ooh, a link!");
         var m = getRandomIntInclusive(0, 4);
-        if (m == 0 || m == 4){
-            message.channel.send("https://");
-        }
-        if (m == 1){
-            message.channel.send("Is that a JOJO reference!!!!");
-        }
-        if (m == 2){
-            message.channel.send("I think thats a rick roll, very sus ಠಿ_ಠ");
-        }
-        if (m == 3){
-            message.channel.send("SSSSSSPPPPPPAAAAAAMMMMM");
-        }
+        getRandomMessage(m);
     }
 })
 
-//random number?
+//function to get a random message for links
+function getRandomMessage(m){
+    if (m == 0){
+        getRandomMessage(getRandomIntInclusive(1,4));
+    }
+    if (m == 4){
+        getRandomMessage(getRandomIntInclusive(0,3));
+    }
+    if (m == 1){
+        message.channel.send("Is that a JOJO reference!!!!");
+    }
+    if (m == 2){
+        message.channel.send("I think thats a rick roll, very sus ಠಿ_ಠ");
+    }
+    if (m == 3){
+        message.channel.send("SSSSSSPPPPPPAAAAAAMMMMM");
+    }
+    return;
+}
+
+//random number
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
