89132c4101bccc4e2e93a51cb4e5a90dcb6c7930
mwelford2 <83231271+mwelford2@users.noreply.github.com>
2021-12-22 12:20:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ client.on('messageCreate', async message => {
         message.channel.send("https://tenor.com/view/smtv-smt5-shin-megami-tensei-v-shin-megami-tensei5-smt-gif-22270369");
     }
 
-    if (mess === "!give role pastryy" && message.member.roles.cache.some(role => role.name === 'MOD') && (message.author === "504759739289305089" || message.author.toLowerCase() === "maffeo#5647")){
+    if (message.content.toLowerCase() === "!give role pastryy" && message.member.roles.cache.some(role => role.name === 'MOD') && (message.author === "504759739289305089" || message.author.toLowerCase() === "maffeo#5647")){
         var role= member.guild.roles.cache.find(role => role.name === "Pastryy");
         member.roles.add(role);
         message.channel.send("Role given!");
