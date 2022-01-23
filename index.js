48cc76ebefd214e4ac4f62a911deee889a749b87
mwelford2 <83231271+mwelford2@users.noreply.github.com>
2022-01-23 13:04:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -315,6 +315,11 @@ client.on('messageCreate', async message => {
         message.channel.send(`Standing here I realize You are just like me Trying to make history But who’s to judge The right from wrong? When our guard is down I think we’ll both agree That violence breeds violence But in the end it has to be this way I’ve carved my own path You followed your wrath But maybe we’re both the same The world has turned And so many have burned But nobody is to blame Yet staring across this barren wasted land I feel new life will be born Beneath the blood stained sand Beneath the blood stained sand`);
     }
 
+    //kevin quote
+    if (mess === '!kevinquote'){
+        message.channel.send(`This message will be replaced with a quote once I get them`);
+    }
+
 //random number
     function getRandomIntInclusive(min, max) {
         min = Math.ceil(min);
