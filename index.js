ad5ff64b78356033ac8b3fb3ca712f34b4fcd8b1
Greman69 <60081167+Greman69@users.noreply.github.com>
2022-06-16 17:56:01 -0400

    Armstrong update
    Its over everybody the funny is no more

--- index.js
+++ index.js
@@ -316,7 +316,7 @@ client.on('messageCreate', async message => {
 
     //message: !armstrong
     if (mess === '!armstrong'){
-        message.channel.send(`Standing here I realize You are just like me Trying to make history But who’s to judge The right from wrong? When our guard is down I think we’ll both agree That violence breeds violence But in the end it has to be this way I’ve carved my own path You followed your wrath But maybe we’re both the same The world has turned And so many have burned But nobody is to blame Yet staring across this barren wasted land I feel new life will be born Beneath the blood stained sand Beneath the blood stained sand`);
+        message.channel.send(`Shut the fuck up! Its not funny anymore!`);
     }
 
     //message: !spam
